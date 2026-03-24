592504105e9d5fa968c322ad43865336ec46af85
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2026-03-24 17:58:33 +0530

    shop-IT-ex3

--- 6-shop-it-v1/src/App.jsx
+++ 6-shop-it-v1/src/App.jsx
@@ -1,17 +1,27 @@
 
 import { useState } from 'react'
+import classNames from 'classnames';
 
 function App() {
   console.log("App Rendered");
   const [currentTab, setCurrentTab] = useState(1);
 
-  const product = {
-    id: 1,
-    name: "Laptop",
-    price: 100000,
-    description: "Laptop for all your computing needs",
-    imageUrl: "/images/Laptop.png"
-  }
+  const products = [
+    {
+      id: 1,
+      name: "Laptop",
+      price: 100000,
+      description: "Laptop for all your computing needs",
+      imageUrl: "/images/Laptop.png"
+    },
+    {
+      id: 2,
+      name: "Smartphone",
+      price: 50000,
+      description: "Smartphone for all your communication needs",
+      imageUrl: "/images/Mobile.png"
+    },
+  ]
 
   const handleTabChange = (tabIndex) => {
     setCurrentTab(tabIndex);
@@ -38,35 +48,41 @@ function App() {
     }
   }
 
+  const renderProducts = () => {
+    return products.map(product => (
+      <div className="list-group-item" key={product.id}>
+        <div className="row">
+          <div className="col-4">
+            <img src={product.imageUrl} className="img-fluid" />
+          </div>
+          <div className="col-8">
+            <div>{product.name}</div>
+            <div>&#8377;{product.price}</div>
+            <button className="btn btn-primary">Add to Cart</button>
+            <ul className="mt-3 nav nav-tabs">
+              <li className="nav-item">
+                <a onClick={() => handleTabChange(1)} className={classNames('nav-link', { active: currentTab === 1 })} href="#">Description</a>
+              </li>
+              <li className="nav-item">
+                <a onClick={() => handleTabChange(2)} className={classNames('nav-link', { active: currentTab === 2 })} href="#">Specification</a>
+              </li>
+              <li className="nav-item">
+                <a onClick={() => handleTabChange(3)} className={classNames('nav-link', { active: currentTab === 3 })} href="#">Reviews</a>
+              </li>
+            </ul>
+            {renderTabPanel(product)}
+          </div>
+        </div>
+      </div>
+    ))
+  }
+
   return (
     <div className="container">
       <div className="display-1">Shop IT</div>
       <hr />
       <div className="list-group">
-        <div className="list-group-item">
-          <div className="row">
-            <div className="col-4">
-              <img src={product.imageUrl} className="img-fluid" />
-            </div>
-            <div className="col-8">
-              <div>{product.name}</div>
-              <div>&#8377;{product.price}</div>
-              <button className="btn btn-primary">Add to Cart</button>
-              <ul className="mt-3 nav nav-tabs">
-                <li className="nav-item">
-                  <a onClick={() => handleTabChange(1)} className="active nav-link" href="#">Description</a>
-                </li>
-                <li className="nav-item">
-                  <a onClick={() => handleTabChange(2)} className="nav-link" href="#">Specification</a>
-                </li>
-                <li className="nav-item">
-                  <a onClick={() => handleTabChange(3)} className="nav-link" href="#">Reviews</a>
-                </li>
-              </ul>
-              {renderTabPanel(product)}
-            </div>
-          </div>
-        </div>
+        {renderProducts()}
       </div>
     </div>
   )
