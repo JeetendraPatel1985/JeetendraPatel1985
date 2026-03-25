d73def9df7a9cb42a4e0121282f0780ae3337781
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2026-03-25 11:05:16 +0530

    shop-IT-ex5

--- 7-shop-it-v2/src/App.jsx
+++ 7-shop-it-v2/src/App.jsx
@@ -1,13 +1,17 @@
 
+import { useState } from 'react';
 import Navbar from './components/Navbar';
 import ProductList from './components/ProductList';
 
 function App() {
   console.log("App Rendered");
+  const [cart, setCart] = useState([]);
 
   return (
     <div className="container">
       <Navbar title="Shop IT" />
+      <hr />
+      <span className="badge bg-danger">{cart.length}</span>  item(s) in cart
       <hr />
       <ProductList />
     </div>
